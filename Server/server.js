5c2691e2288c43e6027ee1dab0ea9e4b7cfc0d28
William B-D <wpfxwill@live.fr>
2017-04-28 10:12:34 -0400

    Ajout de la documentation sur le convertisseur buck

--- Server/server.js
+++ Server/server.js
@@ -10,10 +10,6 @@ require('./routes/routes-config')(app);
 
 app.io.on('connection', function (socket){
    console.log('connection');
-
-  socket.on('CH01', function (from, msg) {
-    console.log('MSG', from, ' saying ', msg);
-  });
 });
 
 http.listen(3000, function () {
